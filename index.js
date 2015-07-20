f6918393be76136590055b7450953d575772256c
Benjamin M. Brown <benjaminbb@gmail.com>
2015-07-20 16:43:34 -0500

    addining initial files

---
+++ index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/*
+*Adds a comma to a number
+*@param [number] number
+*@param [string] locale
+*@return [string]
+*/
+
+module.exports = function(number, locale){
+	return number.toLocaleString(locale);
+}
